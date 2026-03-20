907312042b33ba0408072435eafa85064af0a35e
Amábile Silvério <amabile.silverio34@gmail.com>
2026-03-19 22:17:37 -0300

    Alterando a estrutura

--- public/index.php
+++ public/index.php
@@ -1,4 +1,19 @@
 <?php
+require_once '../app/models/Cidade.php';
+
+//Criando Objeto
+$cidade = new Cidade();
+
+//Atribuindo valores diretamente
+
+$cidade->id = 1;
+$cidade->nome = "Itapira";
+$cidade->estado = "SP";
+
+echo "<pre>";
+var_dump($cidade);
+echo "</pre>";
+
 //index.php será o pronto de entrada do sistema.
 
 echo "<h1>Imobiliaria JG</h1>";
